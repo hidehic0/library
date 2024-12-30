3bc889c66f82b31b260f806ad5032e43aa6e5f7e
hidehic0 <hidehico.ooba@gmail.com>
2024-12-30 21:28:49 +0900

    作成完了

--- code/main.py
+++ code/main.py
@@ -305,6 +305,62 @@ class GraphW:
         return self.grath
 
 
+# UnionFind木
+class UnionFind:
+    """
+    rollbackをデフォルトで装備済み
+    計算量は、経路圧縮を行わないため、基本的なUnionFindの動作は、一回あたり、O(log N)
+    rollbackは、一回あたり、O(1)で行える。
+    """
+
+    def __init__(self, n: int) -> None:
+        self.size = n
+        self.data = [-1] * n
+        self.hist = []
+
+    def root(self, vtx: int) -> int:
+        cur = vtx
+
+        while self.data[cur] >= 0:
+            cur = self.data[cur]
+
+        return cur
+
+    def same(self, a: int, b: int):
+        return self.root(a) == self.root(b)
+
+    def unite(self, a: int, b: int) -> bool:
+        """
+        rootが同じでも、履歴には追加する
+        """
+        ra, rb = self.root(a), self.root(b)
+
+        # 履歴を作成する
+        new_hist = [ra, rb, self.data[ra], self.data[rb]]
+        self.hist.append(new_hist)
+
+        if ra == rb:
+            return False
+
+        if self.data[ra] > self.data[rb]:
+            ra, rb = rb, ra
+
+        self.data[rb] = ra
+        self.data[ra] += self.data[rb]
+
+        return True
+
+    def rollback(self):
+        if not self.hist:
+            return False
+
+        ra, rb, da, db = self.hist.pop()
+        self.data[ra] = da
+        self.data[rb] = db
+        return True
+
+
+# Trie木
 class Trie:
     class Data:
         def __init__(self, value, ind):
