7c6e45d79dece944f3b4f150dd1ee1a4b5c680a8
hidehic0 <hidehico.ooba@gmail.com>
2025-01-07 21:00:43 +0900

    importを分割した

---
+++ python/import.py
@@ -0,0 +1,37 @@
+r"""
+ ______________________
+< it's hidehico's code >
+ ----------------------
+   \
+    \
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/`\
+    \___)=(___/
+"""
+
+# ライブラリと関数と便利変数
+# ライブラリ
+import bisect
+import heapq
+import sys
+import unittest
+from collections import Counter, defaultdict, deque
+from itertools import permutations
+from math import gcd, lcm, pi
+from typing import Any, List
+
+# from atcoder.segtree import SegTree
+# from atcoder.lazysegtree import LazySegTree
+# from atcoder.dsu import DSU
+
+# cortedcontainersは使うときだけ wandbox非対応なので
+# from sortedcontainers import SortedDict, SortedSet, SortedList
+
+# import pypyjit
+# pypyjit.set_param("max_unroll_recursion=-1")
+
+sys.setrecursionlimit(5 * 10**5)
