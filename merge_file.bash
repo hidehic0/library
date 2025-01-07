4a67eadd03db539ed49b565649299620820336c9
hidehic0 <hidehico.ooba@gmail.com>
2025-01-07 22:09:08 +0900

    /bin/catがみつからないようなので修正

--- merge_file.bash
+++ merge_file.bash
@@ -5,12 +5,12 @@ touch code/main.py
 # テンプレ
 # /bin/cat python/<filename>.py >> code/main.py
 
-/bin/cat python/import.py >> code/main.py
-/bin/cat python/math_func.py >> code/main.py
-/bin/cat python/array_create.py >> code/main.py
-/bin/cat python/standard_input.py >> code/main.py
-/bin/cat python/yn_func.py >> code/main.py
-/bin/cat python/memo.py >> code/main.py
-/bin/cat python/grath.py >> code/main.py
-/bin/cat python/unionfind.py >> code/main.py
-/bin/cat python/trie.py >> code/main.py
+cat python/import.py >> code/main.py
+cat python/math_func.py >> code/main.py
+cat python/array_create.py >> code/main.py
+cat python/standard_input.py >> code/main.py
+cat python/yn_func.py >> code/main.py
+cat python/memo.py >> code/main.py
+cat python/grath.py >> code/main.py
+cat python/unionfind.py >> code/main.py
+cat python/trie.py >> code/main.py
