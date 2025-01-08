6f480813d29f4f3b99f9932c29578ab73c92ff25
hidehic0 <hidehic0@users.noreply.github.com>
2025-01-08 05:43:56 +0000

    Merge split file by actions

--- code/main.py
+++ code/main.py
@@ -163,6 +163,33 @@ def create_array3(a: int, b: int, c: int, default: Any = 0) -> List[List[List[An
     ３次元配列を初期化する関数
     """
     return [[[default] * c for _ in [0] * b] for _ in [0] * a]
+from typing import Callable
+
+
+def binary_search(fn: Callable[[int], bool], right: int = 0, left: int = -1) -> int:
+    """
+    二分探索の抽象的なライブラリ
+    評価関数の結果に応じて、二分探索する
+    最終的にはleftを出力します
+
+    関数のテンプレート
+    def check(mid:int):
+        if A[mid] > x:
+            return True
+        else:
+            return False
+
+    midは必須です。それ以外はご自由にどうぞ
+    """
+    while right - left > 1:
+        mid = (left + right) // 2
+
+        if fn(mid):
+            left = mid
+        else:
+            right = mid
+
+    return left
 # 標準入力関数
 import sys
 
