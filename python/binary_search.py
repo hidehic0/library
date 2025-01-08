5315143e7479e87f09a3aef7c82aec61f83d32e2
hidehic0 <hidehico.ooba@gmail.com>
2025-01-08 14:40:42 +0900

    ドキュメントを作成した

--- python/binary_search.py
+++ python/binary_search.py
@@ -2,6 +2,20 @@ from typing import Callable
 
 
 def binary_search(fn: Callable[[int], bool], right: int = 0, left: int = -1) -> int:
+    """
+    二分探索の抽象的なライブラリ
+    評価関数の結果に応じて、二分探索する
+    最終的にはleftを出力します
+
+    関数のテンプレート
+    def check(mid:int):
+        if A[mid] > x:
+            return True
+        else:
+            return False
+
+    midは必須です。それ以外はご自由にどうぞ
+    """
     while right - left > 1:
         mid = (left + right) // 2
 
