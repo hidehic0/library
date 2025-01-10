5c23a3ee3d857eaed2beb944c5ddcf2105dddce9
hidehic0 <hidehico.ooba@gmail.com>
2025-01-10 12:37:16 +0900

    docstringを作成した

--- python/grid.py
+++ python/grid.py
@@ -5,3 +5,27 @@ def coordinate_check(x: int, y: int, H: int, W: int) -> bool:
     """
 
     return 0 <= x < H and 0 <= y < W
+
+
+from typing import List, Tuple
+
+
+def grid_moves(
+    x: int,
+    y: int,
+    H: int,
+    W: int,
+    moves: List[Tuple[int]] = [(0, 1), (0, -1), (1, 0), (-1, 0)],
+    *check_funcs,
+) -> List[Tuple[int]]:
+    """
+    現在の座標から、移動可能な座標をmovesをもとに列挙します。
+    xとyは現在の座標
+    HとWはグリッドのサイズ
+    movesは移動する座標がいくつかを保存する
+    check_funcsは、その座標の点が#だとかを自前で実装して判定はこちらでするみたいな感じ
+    なおcheck_funcsは引数がxとyだけというのが条件
+    """
+    res = []
+
+    return res
