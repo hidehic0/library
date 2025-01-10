a12c9c5625e71dfdc10cc6f4e9dd3b0c664230bf
hidehic0 <hidehic0@users.noreply.github.com>
2025-01-10 11:58:13 +0000

    Format File By Actions

--- code/main.py
+++ code/main.py
@@ -35,6 +35,8 @@ from typing import Any, List
 # pypyjit.set_param("max_unroll_recursion=-1")
 
 sys.setrecursionlimit(5 * 10**5)
+
+
 # 数学型関数
 def is_prime(n):
     if n == 1:
@@ -126,6 +128,8 @@ def factorization(n):
         result.append([n, 1])
 
     return result
+
+
 # 多次元配列作成
 from typing import List, Any
 
@@ -142,6 +146,8 @@ def create_array3(a: int, b: int, c: int, default: Any = 0) -> List[List[List[An
     ３次元配列を初期化する関数
     """
     return [[[default] * c for _ in [0] * b] for _ in [0] * a]
+
+
 from typing import Callable
 
 
@@ -169,6 +175,8 @@ def binary_search(fn: Callable[[int], bool], right: int = 0, left: int = -1) -> 
             right = mid
 
     return left
+
+
 # 標準入力関数
 import sys
 
@@ -196,6 +204,8 @@ def il(add_num: int = 0):
 # 複数行の入力をサポート
 def li(n: int, func, *args):
     return [func(*args) for _ in [0] * n]
+
+
 # YesNo関数
 def YesNoTemplate(state: bool, upper: bool = False) -> str:
     """
@@ -242,6 +252,8 @@ def NE(state: bool, upper: bool = False) -> bool | None:
 
     YN(False, upper)
     exit()
+
+
 def coordinate_check(x: int, y: int, H: int, W: int) -> bool:
     """
     座標がグリッドの範囲内にあるかチェックする関数
@@ -285,6 +297,8 @@ def grid_moves(
             res.append((nx, ny))
 
     return res
+
+
 # ac_libraryのメモ
 """
 segtree
@@ -404,6 +418,8 @@ class GraphW:
     def all(self):
         # グラフの内容をすべて出力
         return self.grath
+
+
 # UnionFind木
 class UnionFind:
     """
@@ -455,6 +471,8 @@ class UnionFind:
         self.data[ra] = da
         self.data[rb] = db
         return True
+
+
 # Trie木
 class Trie:
     class Data:
@@ -522,6 +540,8 @@ class Trie:
             result += self.data[childs[t]].count - 1
 
         return result
+
+
 # 便利変数
 INF = 1 << 63
 lowerlist = list("abcdefghijklmnopqrstuvwxyz")
