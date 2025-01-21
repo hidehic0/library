429d677311a0db743dbae5f25b59bf178c2a44e5
hidehic0 <hidehico.ooba@gmail.com>
2025-01-21 21:08:20 +0900

    mul,divも追加した

--- libs/modint.py
+++ libs/modint.py
@@ -4,3 +4,11 @@ def mod_add(a: int, b: int, mod: int):
 
 def mod_sub(a: int, b: int, mod: int):
     return (a - b) % mod
+
+
+def mod_mul(a: int, b: int, mod: int):
+    return (a * b) % mod
+
+
+def mod_div(a: int, b: int, mod: int):
+    return a * pow(b, mod - 2, mod)
