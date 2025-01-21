9a7d6837bd78f9ebb9ec38b3cdd743949f25a869
hidehic0 <176881051+hidehic0@users.noreply.github.com>
2025-01-21 12:53:09 +0000

    Merge split file by actions

--- code/main.py
+++ code/main.py
@@ -35,8 +35,6 @@ from typing import Any, List
 # pypyjit.set_param("max_unroll_recursion=-1")
 
 sys.setrecursionlimit(5 * 10**5)
-
-
 # 数学型関数
 def is_prime(n):
     if n == 1:
@@ -136,8 +134,6 @@ def simple_sigma(n: int) -> int:
     つまり和の公式
     """
     return (n * (n + 1)) // 2
-
-
 # 多次元配列作成
 from typing import List, Any
 
@@ -154,8 +150,6 @@ def create_array3(a: int, b: int, c: int, default: Any = 0) -> List[List[List[An
     ３次元配列を初期化する関数
     """
     return [[[default] * c for _ in [0] * b] for _ in [0] * a]
-
-
 from typing import Callable
 
 
@@ -183,8 +177,81 @@ def binary_search(fn: Callable[[int], bool], right: int = 0, left: int = -1) -> 
             right = mid
 
     return left
+def mod_add(a: int, b: int, mod: int):
+    return (a + b) % mod
 
 
+def mod_sub(a: int, b: int, mod: int):
+    return (a - b) % mod
+
+
+def mod_mul(a: int, b: int, mod: int):
+    return (a * b) % mod
+
+
+def mod_div(a: int, b: int, mod: int):
+    return (a * pow(b, mod - 2, mod)) % mod
+
+
+class ModInt:
+    def __init__(self, x: int, mod: int = 998244353) -> None:
+        self.x = x % mod
+        self.mod = mod
+
+    def val(self):
+        return self.x
+
+    def rhs(self, rhs) -> int:
+        return rhs.x if isinstance(rhs, ModInt) else rhs
+
+    def __add__(self, rhs) -> int:
+        return mod_add(self.x, self.rhs(rhs), self.mod)
+
+    def __iadd__(self, rhs) -> "ModInt":
+        self.x = self.__add__(rhs)
+
+        return self
+
+    def __sub__(self, rhs) -> int:
+        return mod_sub(self.x, self.rhs(rhs), self.mod)
+
+    def __isub__(self, rhs) -> "ModInt":
+        self.x = self.__sub__(rhs)
+
+        return self
+
+    def __mul__(self, rhs):
+        return mod_mul(self.x, self.rhs(rhs), self.mod)
+
+    def __imul__(self, rhs):
+        self.x = self.__mul__(rhs)
+
+        return self
+
+    def __truediv__(self, rhs):
+        return mod_div(self.x, self.rhs(rhs), self.mod)
+
+    def __itruediv__(self, rhs):
+        self.x = self.__truediv__(rhs)
+
+        return self
+
+    def __floordiv__(self, rhs):
+        return (self.x // self.rhs(rhs)) % self.mod
+
+    def __ifloordiv__(self, rhs):
+        self.x = self.__floordiv__(rhs)
+
+        return self
+
+    def __pow__(self, rhs):
+        return pow(self.x, self.rhs(rhs), self.mod)
+
+    def __eq__(self, rhs) -> bool:
+        return self.rhs(rhs) == self.x
+
+    def __ne__(self, rhs) -> bool:
+        return self.rhs(rhs) != self.x
 # 標準入力関数
 import sys
 
@@ -212,8 +279,6 @@ def il(add_num: int = 0):
 # 複数行の入力をサポート
 def li(n: int, func, *args):
     return [func(*args) for _ in [0] * n]
-
-
 # YesNo関数
 def YesNoTemplate(state: bool, upper: bool = False) -> str:
     """
@@ -260,8 +325,6 @@ def NE(state: bool, upper: bool = False) -> bool | None:
 
     YN(False, upper)
     exit()
-
-
 def coordinate_check(x: int, y: int, H: int, W: int) -> bool:
     """
     座標がグリッドの範囲内にあるかチェックする関数
@@ -305,8 +368,6 @@ def grid_moves(
             res.append((nx, ny))
 
     return res
-
-
 # ac_libraryのメモ
 """
 segtree
@@ -426,8 +487,6 @@ class GraphW:
     def all(self):
         # グラフの内容をすべて出力
         return self.grath
-
-
 # UnionFind木
 class UnionFind:
     """
@@ -479,8 +538,6 @@ class UnionFind:
         self.data[ra] = da
         self.data[rb] = db
         return True
-
-
 # Trie木
 class Trie:
     class Data:
@@ -548,8 +605,6 @@ class Trie:
             result += self.data[childs[t]].count - 1
 
         return result
-
-
 # 便利変数
 INF = 1 << 63
 lowerlist = list("abcdefghijklmnopqrstuvwxyz")
