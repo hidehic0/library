43b72e89760638b5f00eb7a8ecc8a00e06847538
hidehic0 <hidehico.ooba@gmail.com>
2025-01-31 18:26:36 +0900

    マンハッタン距離の関数を追加した

--- libs/dis.py
+++ libs/dis.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def euclid_dis(x1: int, y1: int, x2: int, y2: int) -> int:
     """
     ユークリッド距離を計算します
@@ -8,3 +11,23 @@ def euclid_dis(x1: int, y1: int, x2: int, y2: int) -> int:
     """
 
     return ((x1 - x2) ** 2) + ((y1 - y2) ** 2)
+
+
+def manhattan_dis(x1: int, y1: int, x2: int, y2: int) -> int:
+    """
+    マンハッタン距離を計算します
+    """
+
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def manhattan_45turn(x: int, y: int) -> Tuple[int]:
+    """
+    座標を45度回転します
+    回転すると、マンハッタン距離が、チェビシェフ距離になるので、距離の最大値などが簡単に求められます
+    """
+
+    res_x = x - y
+    res_y = x + y
+
+    return res_x, res_y
