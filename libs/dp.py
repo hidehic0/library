da3038dba0392771f3dddc3d45f04244a0887160
hidehic0 <hidehico.ooba@gmail.com>
2025-02-07 11:19:06 +0900

    コミット忘れた ナップサックと部分和実装完了

--- libs/dp.py
+++ libs/dp.py
@@ -22,3 +22,26 @@ def partial_sum_dp(lis: List[int]) -> List[bool]:
             dp[k + a] = True
 
     return dp
+
+
+def knapsack_dp(lis: List[List[int]], W: int) -> List[int]:
+    """
+    ナップサックdpのテンプレート
+    lisは品物のリスト
+    原則品物は、w,vの形で与えられ、wが重さ、vが価値、となる
+    価値と重さを逆転させたい場合は自分でやってください
+    dp配列は、定数倍高速化のため、一次元配列として扱う
+    dp配列の長さは、Wとします
+    """
+
+    dp = [-(1 << 63)] * (W + 1)
+    dp[0] = 0
+
+    for w, v in lis:
+        for k in reversed(range(len(dp))):
+            if w + k >= len(dp):
+                continue
+
+            dp[w + k] = max(dp[w + k], dp[k] + v)
+
+    return dp
