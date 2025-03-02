4f0d24b0fa29e03205b1a9adaf7612a89963e733
hidehic0 <hidehico.ooba@gmail.com>
2025-03-02 22:55:05 +0900

    例題を変更

--- tests/knapsack_normal.py
+++ tests/knapsack_normal.py
@@ -1,4 +1,4 @@
-# competitive-verifier: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C
+# competitive-verifier: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_B
 from libs.dp import knapsack_dp
 from libs.standard_input import *
 
@@ -6,16 +6,8 @@ from libs.standard_input import *
 def main():
     N, W = il()
     L = [il()[::-1] for _ in [0] * N]
-    NL = []
 
-    for w, v in L:
-        i = 1
-
-        while w * i <= W:
-            NL.append((w * i, v * i))
-            i += 1
-
-    print(knapsack_dp(NL, W))
+    print(knapsack_dp(L, W))
 
 
 if __name__ == "__main__":
