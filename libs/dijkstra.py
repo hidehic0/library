24d7af9ab8f395901c12eb94d56edf36517bf2d7
hidehic0 <hidehico.ooba@gmail.com>
2025-03-06 12:27:50 +0900

    経路復元を実装した

--- libs/dijkstra.py
+++ libs/dijkstra.py
@@ -2,14 +2,18 @@ import heapq
 from typing import List, Tuple
 
 
-def dijkstra(graph: List[List[Tuple[int]]], startpoint: int = 0) -> List[int]:
+def dijkstra(
+    graph: List[List[Tuple[int]]], startpoint: int = 0, output_prev: bool = False
+) -> List[int] | Tuple[List[int], List[int]]:
     """
     ダイクストラ法です
     GraphW構造体を使う場合は、allメソッドで、そんまま入れてください
     定数倍速いのかは分かりません(いつも使っているフォーマット)
+    経路復元したい場合は、output_prevをTrueにすればprevも返ってくるので、それを使用して復元してください
     0-indexedが前提です
     """
     used = [1 << 63] * len(graph)
+    prev = [-1] * len(graph)
     if not 0 <= startpoint < len(graph):
         raise IndexError("あのー0-indexedですか?")
     used[startpoint] = 0
@@ -28,7 +32,26 @@ def dijkstra(graph: List[List[Tuple[int]]], startpoint: int = 0) -> List[int]:
                 continue
 
             used[nxt] = new_cos
+            prev[nxt] = cur
 
             heapq.heappush(PQ, (new_cos, nxt))
 
-    return used
+    if not output_prev:
+        return used
+    else:
+        return used, prev
+
+
+def dijkstra_getpath(prev_lis: List[int], goal_point: int) -> List[int]:
+    """
+    dijkstraの経路復元をします
+    先述のdijkstra関数で、output_prevをTrueにして返ってきた、prevを引数として用います
+    """
+    res = []
+    cur = goal_point
+
+    while cur != -1:
+        res.append(cur)
+        cur = prev_lis[cur]
+
+    return res[::-1]
