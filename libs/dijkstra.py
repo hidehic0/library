41b9006c7ec88be00ce3dc0a2b3fa2ddf88ccbb0
hidehic0 <hidehico.ooba@gmail.com>
2025-03-06 11:56:54 +0900

    動くようにした

--- libs/dijkstra.py
+++ libs/dijkstra.py
@@ -1,6 +1,26 @@
+import heapq
 from typing import List, Tuple
 
 
 def dijkstra(graph: List[List[Tuple[int]]], startpoint: int = 0) -> List[int]:
-    used = [1 << 60] * len(graph)
+    used = [1 << 63] * len(graph)
+    used[startpoint] = 0
+    PQ = [(0, startpoint)]
+
+    while PQ:
+        cos, cur = heapq.heappop(PQ)
+
+        if used[cur] < cos:
+            continue
+
+        for nxt, w in graph[cur]:
+            new_cos = cos + w
+
+            if new_cos >= used[nxt]:
+                continue
+
+            used[nxt] = new_cos
+
+            heapq.heappush(PQ, (new_cos, nxt))
+
     return used
--- tests/dijkstra.py
+++ tests/dijkstra.py
@@ -1,13 +1,20 @@
 # competitive-verifier: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A
 from libs.dijkstra import dijkstra
 from libs.graph import GraphW
-from libs.standard_input import *
+from libs.standard_input import il
+from libs.utils import INF
 
 N, M, S = il()
-G = GraphW(N)
+G = GraphW(N, dire=True)
 
 for _ in [0] * M:
     a, b, w = il()
     G.new_side(a, b, w)
 
-print(*dijkstra(G.all(), S), sep="\n")
+ans = dijkstra(G.all(), S)
+
+for t in ans:
+    if t == INF:
+        print("INF")
+    else:
+        print(t)
