d50f6abf5609c2cc989c612c6399143e6df4e9d0
hidehic0 <hidehico.ooba@gmail.com>
2025-03-06 12:04:13 +0900

    raiseと、docstringを追加した

--- libs/dijkstra.py
+++ libs/dijkstra.py
@@ -3,7 +3,15 @@ from typing import List, Tuple
 
 
 def dijkstra(graph: List[List[Tuple[int]]], startpoint: int = 0) -> List[int]:
+    """
+    ダイクストラ法です
+    GraphW構造体を使う場合は、allメソッドで、そんまま入れてください
+    定数倍速いのかは分かりません(いつも使っているフォーマット)
+    0-indexedが前提です
+    """
     used = [1 << 63] * len(graph)
+    if not 0 <= startpoint < len(graph):
+        raise IndexError("あのー0-indexedですか?")
     used[startpoint] = 0
     PQ = [(0, startpoint)]
 
