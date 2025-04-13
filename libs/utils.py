e41b6882b6be26a702ac869bcb5f37a8fc434949
hidehic0 <hidehico.ooba@gmail.com>
2025-04-13 16:47:14 +0900

    add: movesのプリセットを追加した

--- libs/utils.py
+++ libs/utils.py
@@ -2,5 +2,7 @@
 INF = 1 << 63
 lowerlist = list("abcdefghijklmnopqrstuvwxyz")
 upperlist = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+MOVES1 = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+MOVES2 = MOVES1 + [(1, 1), (1, -1), (-1, 1), (-1, -1)]
 
 # コード
