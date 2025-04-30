10289740c75438a29397d7f2ed07896798c3829e
hidehic0 <hidehico.ooba@gmail.com>
2025-04-30 12:18:41 +0900

    add(cpp): chmin/chmaxを追加

--- code/main.cpp
+++ code/main.cpp
@@ -10,41 +10,46 @@ using ull = unsigned long long;
 #define rep(i, n) for (ll i = 0; i < (int)(n); i++)
 #define all(a) (a).begin(), (a).end()
 
-void printbase() { cout << '\n'; }
-
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
+template <typename T> bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T> bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
 }
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
-    }
-    cout << '\n';
+void printbase() { cout << '\n'; }
+
+template <typename T> void printbase(const T &t) { cout << t << '\n'; }
+
+template <typename T> void printbase(const std::vector<T> &vec) {
+  for (const auto &v : vec) {
+    cout << v << ' ';
+  }
+  cout << '\n';
 }
 
 template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
+void printbase(const Head &head, const Tail &...tail) {
+  cout << head << ' ';
+  printbase(tail...);
 }
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
-    }
+#define print(...)                                                             \
+  {                                                                            \
+    printbase(__VA_ARGS__);                                                    \
+    return 0;                                                                  \
+  }
 
-const ll INF = pow(10, 18);
+const ll INF = (ll)1 << 63;
 const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
 
-int main()
-{
-}
+int main() {}
