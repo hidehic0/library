f8b385bdc811a0f787b946f255b75ff63c278771
hidehic0 <hidehico.ooba@gmail.com>
2025-06-29 14:31:47 +0900

    fix

--- code/main.py
+++ code/main.py
@@ -1473,6 +1473,56 @@ class BIT:
             i += -i & i
 
 
+from typing import Any, Callable
+
+
+class DualSegmentTree:
+    def __init__(self, op: Callable[[Any, Any], Any], e: Any, n: int) -> None:
+        """
+        区間作用/一点取得のセグメント木
+        opは区間作用用の関数
+        eは初期値
+        vは長さ
+        """
+        self._op: Callable[[Any, Any], Any] = op
+        self._e: Any = e
+        self._n: int = n
+        self.n: int = 1 << (n - 1).bit_length()
+        self.data = [e] * (self.n * 2)
+
+    def apply(self, l, r, x) -> None:
+        """
+        区間[l,r)にxを適用
+        """
+        l += self.n
+        r += self.n
+
+        while l < r:
+            if l & 1:
+                self.data[l] = self._op(self.data[l], x)
+                l += 1
+
+            if r & 1:
+                self.data[r - 1] = self._op(self.data[r - 1], x)
+
+            l >>= 1
+            r >>= 1
+
+    def get(self, p: int) -> Any:
+        """
+        pの値を取得する
+        """
+
+        res = self._e
+        p += self.n
+
+        while p:
+            res = self._op(res, self.data[p])
+            p >>= 1
+
+        return res
+
+
 from typing import Tuple
 
 
