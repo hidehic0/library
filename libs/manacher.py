0591f6cf1bd1b63ed0d148fc69e211858d9e568b
hidehic0 <hidehico.ooba@gmail.com>
2025-08-01 20:25:44 +0900

    feat: manacher algorithmのライブラリを追加

---
+++ libs/manacher.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def manacher_algorithm(S: str) -> List[int]:
+    """
+    res_i = S_iを中心とした最長の回文の半径
+    """
+    # いまいち原理は分からないけどうまいことメモ化してそう
+    _n = len(S)
+    res = [0] * _n
+
+    i = k = 0
+
+    while i < _n:
+        while i - k >= 0 and i + k < _n and S[i - k] == S[i + k]:
+            k += 1
+
+        res[i] = k
+        a = 1
+
+        while i - a >= 0 and a + res[i - a] < k:
+            res[i + a] = res[i - a]
+            a += 1
+        i += a
+        k -= a
+
+    return res
---
+++ tests/manacher.py
@@ -0,0 +1,14 @@
+# competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/enumerate_palindromes
+from libs.manacher import manacher_algorithm
+
+S = input()
+T = "$"
+
+for s in S:
+    T += s + "$"
+
+T = T
+
+L = manacher_algorithm(T)
+
+print(*[c - 1 if i % 2 == 0 else c - 1 for i, c in enumerate(L)][1:-1])
